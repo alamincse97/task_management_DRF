2d4804f8f399ff4f07ae1b52c8f1bf81cff1d606
alamincse97 <alamin.cse97@gmail.com>
2024-01-02 16:43:10 +0600

    added to forms

---
+++ tasks/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Task, Photo
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'priority', 'is_complete']
+        widgets = {
+            'due_date': forms.DateTimeInput(altrs={'type': 'datetime-local'}),
+        }
+        
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ['image']
